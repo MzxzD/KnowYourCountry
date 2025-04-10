1c81f86ee765751ba1eca9d68eab10d6f898532a
Mateo Doslic <mateo55@live.com>
2025-04-10 20:36:38 +0200

    Project creation

---
+++ KnowYourCountry/KnowYourCountry/KnowYourCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourCountryApp.swift
+//  KnowYourCountry
+//
+//  Created by Mateo Doslic on 10.04.25.
+//
+
+import SwiftUI
+
+@main
+struct KnowYourCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KnowYourCountry/KnowYourCountryTests/KnowYourCountryTests.swift
@@ -0,0 +1,16 @@
+//
+//  KnowYourCountryTests.swift
+//  KnowYourCountryTests
+//
+//  Created by Mateo Doslic on 10.04.25.
+//
+
+import Testing
+
+struct KnowYourCountryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
