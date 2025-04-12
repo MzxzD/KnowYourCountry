5ed11ba58bf7aec5226b8f4ee4fea7eeacc3ae31
Mateo Doslic <mateo55@live.com>
2025-04-12 09:41:41 +0200

    Initial commit/ create project

---
+++ KnowYourCountry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KnowYourCountry
+//
+//  Created by Mateo Doslic on 12.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KnowYourCountryTests/KnowYourCountryTests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourCountryTests.swift
+//  KnowYourCountryTests
+//
+//  Created by Mateo Doslic on 12.04.25.
+//
+
+import Testing
+@testable import KnowYourCountry
+
+struct KnowYourCountryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
